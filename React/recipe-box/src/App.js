a02704c196eab2cd91e236e710738874fe109367
Chris Scouras <chris.scouras@gmail.com>
2017-05-19 17:11:32 -0700

    Change data source to localStorage

--- React/recipe-box/src/App.js
+++ React/recipe-box/src/App.js
@@ -3,23 +3,39 @@ import Header from './Header';
 import { RecipeContainer, Button } from './RecipeName';
 import './App.css';
 
-const defaultData = [
-  {id: 0,
-  title: "Italian Sausage",
-  ingredients: ["sausage", "peppers", "onions", "garlic"]},
-  {id: 1,
-  title: "Pizza",
-  ingredients: ["dough", "cheese", "sauce"]}
-]
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { recipeList : null }
+  }
 
-const App = () => {
-  return (
-    <div className="app">
-      <Header />
-      <RecipeContainer recipes={ defaultData }/>
-      <Button name="Add Recipe"></Button>
-    </div>
-  )
+  componentWillMount(){
+    if(localStorage.getItem('recipeList') === null){
+      const defaultData = [
+        {id: 0,
+        title: "Italian Sausage",
+        ingredients: ["sausage", "peppers", "onions", "garlic"]},
+        {id: 1,
+        title: "Pizza",
+        ingredients: ["dough", "cheese", "sauce"]}
+      ]
+
+      localStorage.setItem('recipeList', JSON.stringify(defaultData))
+    }
+    const recipes = JSON.parse(localStorage.getItem('recipeList'))
+    this.setState({recipeList: recipes})
+  }
+
+
+  render(){
+    return (
+      <div className="app">
+        <Header />
+        <RecipeContainer recipes={ this.state.recipeList }/>
+        <Button name="Add Recipe"></Button>
+      </div>
+    )
+  }
 }
 
 
