4dc3ded4b63fb32877888f8c59f9cafb83b647b3
Chris Scouras <chris.scouras@gmail.com>
2017-05-21 16:47:10 -0700

    Update App to set / receive data from localStorage

--- React/recipe-box/src/App.js
+++ React/recipe-box/src/App.js
@@ -4,12 +4,39 @@ import { RecipeContainer } from './RecipeContainer';
 import Button from './Button'
 import './App.css';
 
-const App = ({data}) =>
-  <div className="app">
-    <Header />
-    <RecipeContainer recipes={data}/>
-    <Button name="Add Recipe"></Button>
-  </div>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {data : null}
+  }
+
+  componentWillMount() {
+    const DATA = this.props.data
+    let store;
+    if(localStorage.length === 0){
+      localStorage.setItem('recipeList', JSON.stringify(DATA))
+      store = JSON.parse(localStorage.getItem('recipeList'))
+      this.setState({data: store});
+    } else {
+      console.log('Already local storage');
+      store = JSON.parse(localStorage.getItem('recipeList'))
+      this.setState({data: store })
+    }
+    console.log(localStorage.length);
+  }
+
+  render() {
+    console.log(this.state.data, 'DATA LOG');
+    return (
+      <div className="app">
+        <Header />
+        <RecipeContainer recipes={this.state.data}/>
+        <Button name="Add Recipe"></Button>
+      </div>
+    )
+  }
+}
+
 
 
 export default App;
